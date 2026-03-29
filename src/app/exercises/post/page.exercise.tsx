14f23bf0498f94192052110b367f17fbdf3556d8
Kevin <contact@kevinpaulidor.de>
2026-03-29 18:22:12 +0200

    refactor: update post fetching logic and restore rendering of posts

--- src/app/exercises/post/page.exercise.tsx
+++ src/app/exercises/post/page.exercise.tsx
@@ -1,36 +1,36 @@
+'use client'
 import {Post} from '@/lib/type'
-// 🐶 Importe `useEffect` et `useState` car ils seront utiles pour récupérer les datas
-// 🤖 import {useEffect, useState} from 'react'
-
-// 🐶 Une erreur devrait apparaître, corrige la car nous sommes dans un composant client
+import {useEffect, useState} from 'react'
 
 const Page = () => {
   // 🐶 Crée un state pour les posts
-  //const [posts, setPosts] = useState<Post[]>([])
+  const [posts, setPosts] = useState<Post[]>([])
 
   // 🐶 Crée un effet pour récupérer les posts
   // 🤖 Utilise le code ci-dessous
 
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         'https://jsonplaceholder.typicode.com/posts'
-  //       )
-  //       const data = await response.json()
-  //       setPosts(data as Post[])
-  //     } catch (error) {
-  //       console.error('Error fetching posts:', error)
-  //     }
-  //   }
-  //   fetchPosts()
-  // }, [])
+  useEffect(() => {
+    // https://developer.mozilla.org/fr/docs/Web/API/Fetch_API/Using_Fetch
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(
+          'http://localhost:4000/posts'
+        )
+        if (!response.ok) throw new Error(`HTTP ${response.status}`)
+        const data = await response.json()
+        setPosts(data as Post[])
+      } catch (error) {
+        console.error('Error fetching posts:', error)
+      }
+    }
+    fetchPosts()
+  }, [])
   return (
     <div className="mx-auto max-w-4xl p-6 text-lg">
       <h1 className="mb-4 text-center text-3xl font-bold"> Fetch Posts</h1>
       <ul className="list-disc p-4 pl-4">
         {/* 🐶 Affiche les posts */}
-        {/* {posts?.map((post: Post) => <li key={post.title}>{post.title}</li>)} */}
+        {posts?.map((post: Post) => <li key={post.title}>{post.title}</li>)}
       </ul>
     </div>
   )
