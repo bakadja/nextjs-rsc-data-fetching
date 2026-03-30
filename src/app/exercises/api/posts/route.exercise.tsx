23b4da189ed3bf8203ce580a5a19ce5855713c9e
Kevin <contact@kevinpaulidor.de>
2026-03-30 11:24:45 +0200

    fix: enable post retrieval in route handler

--- src/app/exercises/api/posts/route.exercise.tsx
+++ src/app/exercises/api/posts/route.exercise.tsx
@@ -1,12 +1,12 @@
 // 🐶 Importe la fonction `getPosts` qui va récupérer les posts en BDD
-//import {getPosts} from '@/db/sgbd'
+import {getPosts} from '@/db/sgbd'
 
 export async function GET() {
   // 🐶 Appelle la fonction `getPosts` pour récupérer les posts en BDD
-
+const posts = await getPosts()
   // 🐶 Retourne les données en json dans la réponse http
   // ⛏️ Supprime `[]` et remplace le par la `data`
-  return Response.json([])
+  return Response.json(posts)
   // 🐶 Constate la présence des données ici:  http://localhost:3000/exercises/api/posts
 
   // 🐶 N'oublie pas les exercices bonus
