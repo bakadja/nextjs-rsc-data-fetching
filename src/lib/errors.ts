785cf6da5d1498b84c28d66f91e85cf85548d98e
Kevin <contact@kevinpaulidor.de>
2026-04-08 17:19:43 +0200

    feat: add custom error classes for ValidationError and DatabaseError

---
+++ src/lib/errors.ts
@@ -0,0 +1,12 @@
+export class ValidationError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'ValidationError'
+  }
+}
+export class DatabaseError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'DatabaseError'
+  }
+}
