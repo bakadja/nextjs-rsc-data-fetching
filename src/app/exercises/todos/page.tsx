94648f152b9527625329770a944d5c459039c848
Kevin <contact@kevinpaulidor.de>
2026-04-08 13:30:06 +0200

    refactor: rename getTodos function and implement caching with Suspense for improved loading state

--- src/app/exercises/todos/page.tsx
+++ src/app/exercises/todos/page.tsx
@@ -1,14 +1,27 @@
-import {getTodos} from '@/db/sgbd'
+import {getTodos as getTodosDao} from '@/db/sgbd'
 import Todos from './todos-view'
+import {cacheLife, cacheTag} from 'next/cache'
+import {Suspense} from 'react'
 
 //export const revalidate = 3600
+
+async function GetTodos() {
+  'use cache'
+  cacheTag('todos')
+  cacheLife('seconds')
+  const todos = await getTodosDao()
+
+  return <Todos todos={todos ?? []} />
+}
+
 const Page = async () => {
-  const todos = await getTodos()
   return (
-    <div className="mx-auto max-w-4xl p-6 text-lg">
-      <h1 className="mb-4 text-center text-3xl font-bold">Todo</h1>
-      <Todos todos={todos ?? []} />
-    </div>
+    <Suspense fallback={<p>Loading ...</p>}>
+      <div className="mx-auto max-w-4xl p-6 text-lg">
+        <h1 className="mb-4 text-center text-3xl font-bold">Todo</h1>
+        <GetTodos />
+      </div>
+    </Suspense>
   )
 }
 
