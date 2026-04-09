2829ba0d452ecabccc99754d17da1e9f26cfbc6b
Kevin <contact@kevinpaulidor.de>
2026-04-09 17:36:04 +0200

    fix: update cacheLife parameter to 'seconds' in GetTodos function

--- src/app/exercises/todos/page.tsx
+++ src/app/exercises/todos/page.tsx
@@ -8,7 +8,7 @@ import {Suspense} from 'react'
 async function GetTodos() {
   'use cache'
   cacheTag('todos')
-  cacheLife('default')
+  cacheLife('seconds')
   const todos = await getTodosDao()
 
   return <Todos todos={todos ?? []} />
